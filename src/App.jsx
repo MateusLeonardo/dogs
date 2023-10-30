971db0b05d0129e6d2fbc681b61e6fec8fe96238
Mateus <mateus_leonardo1997@hotmail.com>
2023-10-30 07:29:49 -0300

    adicionando useForm, UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css'
+import "./App.css";
 import Header from "./Componentes/Header/Header";
 import Footer from "./Componentes/Footer/Footer";
 import Home from "./Componentes/Home/Home";
 import Login from "./Componentes/Login/Login";
+import { UserStorage } from "./UserContext";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
